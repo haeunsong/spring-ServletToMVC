ba084a2ed9496c6940d2c63f275a6cc4f3be7c00
haeunsong <jamy022576@gmail.com>
2024-12-08 00:07:53 +0900

    HTTP 요청 데이터 - API 메시지 바디 (단순텍스트, JSON)

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+// 결과 : messageBody = hello!
+// getInputStream() 으로 문자열을 읽을 수 있다.
+// 하지만 요즘은 거의 대부분 JSON 으로 문자열을 주고 받는다.
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream inputStream = req.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+        resp.getWriter().write("ok");
+    }
+}
